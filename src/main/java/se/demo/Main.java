4fe2d3dce6268c9973e69089c095d8d2faf8552c
Ali Rezai <ali.rezai27461@gmail.com>
2024-11-27 11:50:16 +0100

    :poop:

--- src/main/java/se/demo/Main.java
+++ src/main/java/se/demo/Main.java
@@ -5,15 +5,19 @@ import se.demo.config.DatabaseConnectionFactory;
 import se.demo.dao.TodoDAO;
 import se.demo.dao.impl.TodoDAOImpl;
 import se.demo.service.TodoService;
-import se.demo.ui.ChoiceDatabase;
+import se.demo.ui.DatabaseChoice;
 import se.demo.ui.MenuSystem;
 
 
 public class Main {
     public static void main(String[] args) {
 
-       ChoiceDatabase choiceDatabase = new ChoiceDatabase();
-        int choice = choiceDatabase.choiceDatabase();
+        DatabaseChoice databaseChoice = new DatabaseChoice();
+        int choice = databaseChoice.databaseChoice();
+        if(choice == 3){
+            System.out.println("Avsluta");
+            return;
+        }
         DatabaseConnection databaseConnection = DatabaseConnectionFactory.createDatabaseConnection(choice);
         System.out.println(databaseConnection.getSessionFactory());
         if(databaseConnection.getSessionFactory() == null){
--- src/main/java/se/demo/ui/DatabaseChoice.java
+++ src/main/java/se/demo/ui/DatabaseChoice.java
@@ -2,16 +2,15 @@ package se.demo.ui;
 
 import java.util.Scanner;
 
-public class ChoiceDatabase {
-    public int choiceDatabase(){
-
+public class DatabaseChoice {
+    public int databaseChoice(){
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("\n╔════════════════════════════╗");
         System.out.println("║        Välja databas       ║");
         System.out.println("╠════════════════════════════╣");
         System.out.println("║ 1. MySql                   ║");
         System.out.println("║ 2. H2                      ║");
+        System.out.println("║ 3. Avsluta                 ║");
         System.out.println("╚════════════════════════════╝");
         System.out.print("Välj ett alternativ: ");
         int choice = scanner.nextInt();
